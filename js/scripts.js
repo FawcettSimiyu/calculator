c9e50d37366bb6e1435eef2a6fc9b6912e8b207e
blackbad1 <fawcetteugene@gmail.com>
2019-07-11 10:12:49 +0300

    Calculato code added.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var add=function (number1,number2){
+  return number1 + number2;
+};
+var number1=parseInt(prompt("Enter Number: "));
+var number2=parseInt(prompt("Enter Number: "));
+var result=add(number1,number2);
+alert(result);
+
+var sub=function (number1,number2){
+  return number1 - number2;
+};
+var number1=parseInt(prompt("Enter Number: "));
+var number2=parseInt(prompt("Enter Number: "));
+var result=sub(number1,number2);
+alert(result);
+
+var multi=function (number1,number2){
+  return number1 * number2;
+};
+var number1=parseInt(prompt("Enter Number: "));
+var number2=parseInt(prompt("Enter Number: "));
+var result=multi(number1,number2);
+alert(result);
+
+var divide=function (number1,number2){
+  return number1/number2;
+};
+var number1=parseInt(prompt("Enter Number: "));
+var number2=parseInt(prompt("Enter Number: "));
+var result=divide(number1,number2);
+alert(result);
